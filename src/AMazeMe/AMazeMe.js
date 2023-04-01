2f45f16b2306fa180f20090b114ed27f85df6e86
Andrew Diles <a_diles@hotmail.com>
2023-04-01 07:14:31 -0400

    App and Component Generator

--- src/AMazeMe/AMazeMe.js
+++ src/AMazeMe/AMazeMe.js
@@ -1,6 +1,18 @@
+import { RecordsProvider } from "./contexts/RecordsContext";
+import { LevelProvider } from "./contexts/LevelContext";
+import { GameProvider } from "./contexts/GameContext";
+import GlobalStyles from "./components/GlobalStyles"
+import ComponentGenerator from "./components/ComponentGenerator";
 
 export default () => {
   return (
-    <main>make a maze</main>
+    <GameProvider>
+      <LevelProvider>
+        <RecordsProvider>
+					<GlobalStyles/>
+          <ComponentGenerator />
+        </RecordsProvider>
+      </LevelProvider>
+    </GameProvider>
   );
 };
