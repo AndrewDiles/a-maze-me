d5d425a3ea5f524433b33565a6719d9c155c5c55
Andrew Diles <a_diles@hotmail.com>
2023-04-01 07:10:37 -0400

    constants

---
+++ src/AMazeMe/constants/levelEditor.js
@@ -0,0 +1,7 @@
+const tileSizeMultiplier = 0.4;
+const gapSizeMultiplier = 0.08;
+const remainingSize = 1.5 - 3 * tileSizeMultiplier - 2 * gapSizeMultiplier;
+const containerMarginSizeMultiplier = remainingSize / 2;
+const sizeWhenNotOnRight = "60px";
+
+export {tileSizeMultiplier, gapSizeMultiplier, containerMarginSizeMultiplier, sizeWhenNotOnRight}
---
+++ src/AMazeMe/constants/requiredLevelKeys.js
@@ -0,0 +1,14 @@
+export default [
+	"name",
+	"rows",
+	"cols",
+	"layout",
+	"borderColor",
+	"playerColor",
+	"goalColor",
+	"barrier1Color",
+	"barrier2Color",
+	"barrier3Color",
+	"floorColor",
+	"enemyColor"
+]
