1c0d9ea643a6e878ec3fac680052d2705e977002
Andrew Diles <a_diles@hotmail.com>
2023-04-02 07:02:35 -0400

    Component Generator: case maze

--- src/AMazeMe/components/ComponentGenerator.js
+++ src/AMazeMe/components/ComponentGenerator.js
@@ -3,24 +3,35 @@ import GameContext from "../contexts/GameContext";
 import SlotSelection from "./SlotSelection";
 import Worlds from "./Worlds";
 import Menu from "./Menu";
-// import Maze from "./Maze";
+import Maze from "./Maze";
 import ImportCustom from "./ImportCustom";
 import LevelEditor from "./LevelEditor";
 import Records from "./Records";
 
 export default () => {
-	const {game} = useContext(GameContext)
-	
-	switch (game.viewing) {
-		case "menu" : return <Menu/>
-		case "worlds" : {
-			if (game.subView === "select-slot") return <SlotSelection/>
-			else return <Worlds/>
-		}
-		case "editor" : {
-			return game.subView === "import" ? <ImportCustom/> : <LevelEditor/>
-		}
-		case "records" : return <Records/>
-		default : return <h3>This isn't the component you're looking for.  Derived from {game.viewing}, sub: {game.subView}</h3>
-	}
+  const { game } = useContext(GameContext);
+
+  switch (game.viewing) {
+    case "menu":
+      return <Menu />;
+    case "worlds": {
+      if (game.subView === "select-slot") return <SlotSelection />;
+      else return <Worlds />;
+    }
+    case "maze": {
+      return <Maze />;
+    }
+    case "editor": {
+      return game.subView === "import" ? <ImportCustom /> : <LevelEditor />;
+    }
+    case "records":
+      return <Records />;
+    default:
+      return (
+        <h3>
+          This isn't the component you're looking for. Derived from{" "}
+          {game.viewing}, sub: {game.subView}
+        </h3>
+      );
+  }
 };
