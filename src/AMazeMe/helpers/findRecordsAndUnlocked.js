9da99f6b8d86ca8e2e2a06903030cfcd7fd41b93
Andrew Diles <a_diles@hotmail.com>
2023-04-02 07:03:27 -0400

    find records and unlocked helper

---
+++ src/AMazeMe/helpers/findRecordsAndUnlocked.js
@@ -0,0 +1,31 @@
+export default ({selectedRecords, selection}) => {
+
+	const worldProgressArray = selectedRecords[selection.worldIndex];
+
+	const records = worldProgressArray
+		? worldProgressArray[selection.levelIndex]
+			? worldProgressArray[selection.levelIndex]
+			: null
+		: null;
+	
+	let unlocked = false;
+	if (selection.levelIndex === 0) {
+		if (selection.worldIndex === 0) {
+			unlocked = true;
+		} else if (
+			selectedRecords[selection.worldIndex - 1] &&
+			selectedRecords[selection.worldIndex - 1].length === 9
+		) {
+			unlocked = true;
+		}
+	} else {
+		if (
+			selectedRecords[selection.worldIndex - 1] &&
+			selectedRecords[selection.worldIndex - 1][selection.levelIndex - 1]
+		) {
+			unlocked = true;
+		}
+	}
+
+	return {records, unlocked}
+}
