9f6cb1a7ca1deff8993bdbeb8f65053ec9c114fe
Andrew Diles <a_diles@hotmail.com>
2023-04-04 21:22:08 -0400

    controls hook

---
+++ src/AMazeMe/hooks/useMovementControls.js
@@ -0,0 +1,62 @@
+import { useContext, useEffect } from "react";
+import GameContext from "../contexts/GameContext";
+
+const useWASDSelectedCell = () => {
+  const {
+    upPress,
+    downPress,
+    leftPress,
+    rightPress,
+    upUnpress,
+    downUnpress,
+    leftUnpress,
+    rightUnpress,
+  } = useContext(GameContext);
+
+  useEffect(() => {
+    const handleKeyDown = ({ key }) => {
+      // console.log(key);
+      switch (key) {
+        case "w":
+        case "ArrowUp":
+          return upPress();
+        case "a":
+        case "ArrowLeft":
+          return leftPress();
+        case "s":
+        case "ArrowDown":
+          return downPress();
+        case "d":
+        case "ArrowRight":
+          return rightPress();
+      }
+    };
+		const handleKeyUp = ({ key }) => {
+      switch (key) {
+        case "w":
+        case "ArrowUp":
+          return upUnpress();
+        case "a":
+        case "ArrowLeft":
+          return leftUnpress();
+        case "s":
+        case "ArrowDown":
+          return downUnpress();
+        case "d":
+        case "ArrowRight":
+          return rightUnpress();
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+		window.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+			window.removeEventListener("keyup", handleKeyUp);
+    };
+  });
+
+  return null;
+};
+
+export default useWASDSelectedCell;
