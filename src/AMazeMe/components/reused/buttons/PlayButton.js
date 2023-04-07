63fc92fa939e99f255ecef2008fc695fb38756de
Andrew Diles <a_diles@hotmail.com>
2023-04-06 20:53:55 -0400

    Play Button focus

--- src/AMazeMe/components/reused/buttons/PlayButton.js
+++ src/AMazeMe/components/reused/buttons/PlayButton.js
@@ -1,9 +1,13 @@
 import { useContext } from "react";
 import GameContext from "../../../contexts/GameContext";
 import Button from "./Button";
+import useFocusIdOnMount from "../../../hooks/useFocusIdOnMount";
+
+const idToFocus = "play-button"
 
 export default ({worldToPlay, levelToPlay, disabled}) => {
   const { playLevel } = useContext(GameContext);
+	useFocusIdOnMount(idToFocus);
 
-  return <Button disabled = {disabled} onClick={()=>{playLevel({worldToPlay, levelToPlay})}}>{disabled ? "locked":"play level"}</Button>;
+  return <Button id = {!disabled && idToFocus} disabled = {disabled} onClick={()=>{playLevel({worldToPlay, levelToPlay})}}>{disabled ? "locked":"play level"}</Button>;
 };
