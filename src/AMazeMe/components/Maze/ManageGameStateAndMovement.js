a8283c0d863e049425c3304a00ac734bf5f66a7f
Andrew Diles <a_diles@hotmail.com>
2023-04-06 20:55:52 -0400

    game state controller and movement

--- src/AMazeMe/components/Maze/ManageGameStateAndMovement.js
+++ src/AMazeMe/components/Maze/ManageGameStateAndMovement.js
@@ -1,10 +1,10 @@
-import { useContext, useEffect, useRef } from "react";
+import { useState, useContext, useEffect, useRef } from "react";
 import LevelContext from "../../contexts/LevelContext";
 import GameContext from "../../contexts/GameContext";
 import useMovementControls from "../../hooks/useMovementControls";
 import useMovementAndCollisions from "../../hooks/useMovementAndCollisions";
-import styled from "styled-components";
-import { DELAY } from "../../constants/general";
+import styled, { keyframes } from "styled-components";
+import { GAME_FREQ, WIN_DELAY } from "../../constants/general";
 
 export default () => {
   const board = useRef(null);
@@ -12,6 +12,8 @@ export default () => {
   const { playerColor, goalColor } = level;
   const { game, startTimer, winGame } = useContext(GameContext);
   const { size } = dimensions;
+	const [finishHandled, setFinishHandled] = useState(false)
+
   useEffect(() => {
     board.current = document.getElementById("board");
     startTimer();
@@ -22,28 +24,31 @@ export default () => {
       console.log("game has begun");
     }
   }, [game.startTime]);
-	
-	useEffect(()=>{
-		if (game.finishTime) {
-			setTimeout(winGame, 2000)
-		}
-	}, [game.finishTime])
+
+  useEffect(() => {
+    if (game.finishTime && !finishHandled) {
+			setFinishHandled(true);
+      setTimeout(winGame, WIN_DELAY);
+    }
+  }, [game.finishTime]);
 
   useMovementControls();
   // const hasEnded =
   //   game.players.every((player) => !player.alive) || game.finishTime > 0;
-	
-  useMovementAndCollisions({ hasStarted : game.startTime > 0, hasEnded: game.ended });
+
+  useMovementAndCollisions({
+    hasStarted: game.startTime > 0,
+    hasEnded: game.ended,
+  });
 
   if (!board.current) return null;
 
-  const rects = board.current.getBoundingClientRect();
-  const { x, y } = rects;
+  const x = board.current.getBoundingClientRect().x;
   return (
     <>
       {game.players.map((playerInfo, index) => {
-        const top = y + size / 4 + size * (playerInfo.y / 100);
         const left = x + size / 4 + size * (playerInfo.x / 100);
+        const top = size / 4 + size * (playerInfo.y / 100);
         return playerInfo.alive ? (
           <Player
             key={index}
@@ -51,11 +56,13 @@ export default () => {
             top={top}
             left={left}
             size={size}
+            animation={playerInfo.animation}
+            WIN_DELAY={WIN_DELAY}
           />
         ) : null;
       })}
       {game.goals.map((goalInfo, index) => {
-        const top = y + size / 4 + size * (goalInfo.y / 100);
+        const top = size / 4 + size * (goalInfo.y / 100);
         const left = x + size / 4 + size * (goalInfo.x / 100);
         return (
           <Goal
@@ -64,6 +71,8 @@ export default () => {
             top={top}
             left={left}
             size={size}
+            animation={goalInfo.animation}
+            WIN_DELAY={WIN_DELAY}
           />
         );
       })}
@@ -71,21 +80,53 @@ export default () => {
   );
 };
 
+const popping = keyframes`
+ 0% { filter: blur(0px) }
+ 25% { filter: blur(10px) }
+ 100% { filter: blur(100px)}
+`;
+const win = keyframes`
+ 0% { 
+	outline: 0em solid var(--color-a);
+	filter: hue-rotate(0deg)
+	border-radius: 0 0 0 0;
+}
+ 25% { outline: .25em solid var(--color-a);
+	border-radius: 50% 0 0 0;
+ }
+ 50% {
+	border-radius: 50% 50% 0 0;
+ }
+ 75% { outline: .5em solid var(--color-h);
+	border-radius: 50% 50% 50% 0;
+}
+ 100% { outline: .1em solid var(--color-f);
+	filter: hue-rotate(360deg);
+	border-radius: 50%;
+ }
+`;
+
 const Square = styled.div.attrs((props) => ({
   style: {
     backgroundColor: props.color,
     transform: `translate(${props.left}px, ${props.top}px)`,
     height: `${props.size / 2}px`,
     width: `${props.size / 2}px`,
-    transitionDuration: `${DELAY}ms`,
+    transitionDuration: `${GAME_FREQ}ms`,
   },
 }))`
+  transform-origin: center;
   position: absolute;
   top: 0;
   left: 0;
   z-index: 10;
   transition-property: transform;
   transition-timing-function: ease-in-out;
+  animation-duration: ${({ WIN_DELAY }) => WIN_DELAY}ms;
+  /* options are win, damage, sparkles, popping */
+  /* animation: ${({ animation }) => animation && `${animation}`}; */
+  animation-name: ${({ animation }) =>
+    animation === "popping" ? popping : animation === "win" ? win : ""};
 `;
 
 const Player = styled(Square)`
