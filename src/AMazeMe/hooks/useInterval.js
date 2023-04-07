8a2230b16f43db99f332e78fb605694c03673640
Andrew Diles <a_diles@hotmail.com>
2023-04-07 16:05:16 -0400

    useInterval hook

--- src/AMazeMe/hooks/useInterval.js
+++ src/AMazeMe/hooks/useInterval.js
@@ -1,21 +1,22 @@
 import { useEffect, useRef } from "react";
 
-const useInterval = ({cb, delay}) => {
-    const cbRef = useRef();
+const useInterval = ({ cb, delay }) => {
+  const cbRef = useRef();
 
-    useEffect(() => {
-			cbRef.current = cb;
-    }, [cb]);
+  useEffect(() => {
+    cbRef.current = cb;
+  }, [cb]);
 
-    useEffect(() => {
-        const update = () => {
-					cbRef.current();
-        }
-        if (delay > 0) {
-            let interval = setInterval(update, delay);
-            return () => clearInterval(interval);
-        }
-    }, [delay]);
-}
+  useEffect(() => {
+    let interval;
+    const update = () => {
+      cbRef.current();
+    };
+    if (delay > 0) {
+      interval = setInterval(update, delay);
+    }
+    return () => clearInterval(interval);
+  }, [delay]);
+};
 
 export default useInterval;
