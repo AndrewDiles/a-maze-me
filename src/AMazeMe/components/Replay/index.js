6940b45536c40dbc9eab683d980b50bf41552eff
Andrew Diles <a_diles@hotmail.com>
2023-04-12 21:14:33 -0400

    Replay cleanup

--- src/AMazeMe/components/Replay/index.js
+++ src/AMazeMe/components/Replay/index.js
@@ -1,6 +1,10 @@
 import { useEffect, useContext } from "react";
 import GameContext from "../../contexts/GameContext";
 
+// This exists purely as a cheap way to restart a game without having to re-code the state logic.
+// As this component will only be mounted if Maze was unmounted.
+// playLevel will then trigger Maze to remount, and re-execute the game initialization.
+
 export default () => {
   const { playLevel, game: { world, level } } = useContext(GameContext);
   useEffect(() => {
