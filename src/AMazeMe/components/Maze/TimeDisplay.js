3e6eb538f3532cd0a8af8326b427af9f9ce69fca
Andrew Diles <a_diles@hotmail.com>
2023-04-12 21:13:56 -0400

    Maze refactor

--- src/AMazeMe/components/Maze/TimeDisplay.js
+++ src/AMazeMe/components/Maze/TimeDisplay.js
@@ -1,7 +1,13 @@
+import { useContext } from "react";
+import LevelContext from "../../contexts/LevelContext";
+import GameContext from "../../contexts/GameContext";
 import formatTime from "../../helpers/formatTime";
 import styled from "styled-components";
 
-export default ({ startTime, finishTime, target }) => {
+export default () => {
+  const { game } = useContext(GameContext);
+  const { level: { target } } = useContext(LevelContext);
+  const { startTime, finishTime } = game;
   const finish = finishTime > 0 ? finishTime : Date.now();
   const time = formatTime(finish - startTime);
   const metTarget = finishTime && time < target;
@@ -17,6 +23,7 @@ const Time = styled.span`
   position: absolute;
   left: 0.5em;
   bottom: 0;
+	z-index: 5;
   & span {
     color: ${({ metTarget }) => metTarget && "var(--color-accent)"};
   }
